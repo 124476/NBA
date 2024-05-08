ff1b66725643a1270c7ef1fd66334fd7b57e76ce
124476 <124476@bk.ru>
2024-05-08 19:35:56 +0300

    First Version

---
+++ NBA/Models/Partials/Mathup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBA.Models
+{
+    public partial class Matchup
+    {
+        public string StatusName
+        {
+            get
+            {
+                if (Status == 1)
+                    return "Finished";
+                return "No Start";
+            }
+        }
+        public string Result
+        {
+            get
+            {
+                return Team_Home_Score.ToString() + " - " + Team_Away_Score.ToString();
+            }
+        }
+    }
+}
--- NBA/Pages/PageDetail.xaml.cs
+++ NBA/Pages/PageDetail.xaml.cs
@@ -21,11 +21,11 @@ namespace NBA.Pages
     /// </summary>
     public partial class PageDetail : Page
     {
-        Team contextText;
+        Team contextTeam;
         public PageDetail(Team team)
         {
             InitializeComponent();
-            var dates = new List<string>();
+            var dates = new List<string>() { "All" };
             DateTime dateStart = DateTime.Now.AddYears(-24);
             DateTime dateEnd = DateTime.Now;
             while (dateEnd > dateStart)
@@ -35,28 +35,59 @@ namespace NBA.Pages
             }
 
             PoiskCombo.ItemsSource = dates;
-            contextText = team;
-            DataPlayers.ItemsSource = App.DB.PlayerInTeam.Where(x => x.TeamId == team.TeamId).Select(x => x.Player).ToList();
+            contextTeam = team;
+            var players = App.DB.PlayerInTeam.Where(x => x.TeamId == team.TeamId).Select(x => x.Player).ToList();
+            var playerss = new List<Player>();
+            foreach (var item in players)
+            {
+                if (playerss.Contains(item))
+                    continue;
+                playerss.Add(item);
+            }
+            DataPlayers.ItemsSource = playerss;
+            DataMathups.ItemsSource = App.DB.Matchup.Where(x => x.Team.TeamId == team.TeamId).ToList();
+            SG.ItemsSource = playerss.Where(x => x.PositionId == 4).ToList();
+            PF.ItemsSource = playerss.Where(x => x.PositionId == 2).ToList();
+            C.ItemsSource = playerss.Where(x => x.PositionId == 3).ToList();
+            SF.ItemsSource = playerss.Where(x => x.PositionId == 1).ToList();
+            PG.ItemsSource = playerss.Where(x => x.PositionId == 5).ToList();
+            DataMathups.Visibility = Visibility.Collapsed;
+            LineupGrid.Visibility = Visibility.Collapsed;
+            DataContext = contextTeam;
         }
 
         private void Roster_Click(object sender, RoutedEventArgs e)
         {
             DataPlayers.Visibility = Visibility.Visible;
+            DataMathups.Visibility = Visibility.Collapsed;
+            LineupGrid.Visibility = Visibility.Collapsed;
         }
 
         private void Mathup_Click(object sender, RoutedEventArgs e)
         {
-            DataPlayers.Visibility = Visibility.Hidden;
+            DataPlayers.Visibility = Visibility.Collapsed;
+            DataMathups.Visibility = Visibility.Visible;
+            LineupGrid.Visibility = Visibility.Collapsed;
         }
 
         private void Lineup_Click(object sender, RoutedEventArgs e)
         {
-            DataPlayers.Visibility = Visibility.Hidden;
+            DataPlayers.Visibility = Visibility.Collapsed;
+            DataMathups.Visibility = Visibility.Collapsed;
+            LineupGrid.Visibility = Visibility.Visible;
         }
 
         private void SearhBtn_Click(object sender, RoutedEventArgs e)
         {
+            if (PoiskCombo.SelectedIndex == -1 || PoiskCombo.Text == "All")
+            {
+                DataMathups.ItemsSource = App.DB.Matchup.Where(x => x.Team.TeamId == contextTeam.TeamId).ToList();
+                return;
+            }
 
+            DateTime dateStart = new DateTime();
+            dateStart = dateStart.AddYears(Int32.Parse(PoiskCombo.Text.Split('-')[0]) - 1);
+            DataMathups.ItemsSource = App.DB.Matchup.ToList().Where(x => x.Team.TeamId == contextTeam.TeamId && x.Starttime >= dateStart && x.Starttime <= dateStart.AddYears(1)).ToList();
         }
     }
 }
