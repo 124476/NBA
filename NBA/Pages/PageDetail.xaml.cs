7627a7be63ab7d5cfdfbbb7e1c981e1ce6426751
124476 <124476@bk.ru>
2024-05-08 18:39:22 +0300

    Two Version

--- NBA/Pages/PageDetail.xaml.cs
+++ NBA/Pages/PageDetail.xaml.cs
@@ -25,6 +25,16 @@ namespace NBA.Pages
         public PageDetail(Team team)
         {
             InitializeComponent();
+            var dates = new List<string>();
+            DateTime dateStart = DateTime.Now.AddYears(-24);
+            DateTime dateEnd = DateTime.Now;
+            while (dateEnd > dateStart)
+            {
+                dates.Add(dateStart.Year.ToString() + " - " + dateStart.AddYears(1).Year.ToString());
+                dateStart = dateStart.AddYears(1);
+            }
+
+            PoiskCombo.ItemsSource = dates;
             contextText = team;
             DataPlayers.ItemsSource = App.DB.PlayerInTeam.Where(x => x.TeamId == team.TeamId).Select(x => x.Player).ToList();
         }
@@ -42,6 +52,11 @@ namespace NBA.Pages
         private void Lineup_Click(object sender, RoutedEventArgs e)
         {
             DataPlayers.Visibility = Visibility.Hidden;
+        }
+
+        private void SearhBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
